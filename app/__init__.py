c02c793c0f5d37af067626994b4612e9e710ea88
Peiman Nourani <nourani.peiman@gmail.com>
2019-02-01 20:44:07 +0330

    Add telegram bot package

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask
 from flask_bcrypt import Bcrypt
+import telegram
+
 sec = 'asdaskjdhaufsf897987'
 # Initialize application
 app = Flask(__name__, static_folder=None)
@@ -15,8 +17,20 @@ app.config.from_object(app_settings)
 # Initialize Bcrypt
 bcrypt = Bcrypt(app)
 
+bot = telegram.Bot(app.config.TOKEN)
 
 # Edit here below
 @app.route("/"+sec)
 def hello():
     return "Hello World!"
+
+@app.route('/' + sec, methods=['POST'])
+def webhook():
+    update = telegram.update.Update.de_json(request.get_json(force=True))
+    bot.sendMessage(chat_id=update.message.chat_id, text='Hello, there')
+
+    return 'OK'
+
+def setWebhook():
+    bot.setWebhook(webhook_url='https://%s:%s/%s' % (app.config.DevelopmentConfig.HOST, app.config.DevelopmentConfig.PORT, sec),
+                   certificate=open(CERT, 'rb'))
--- app/config.py
+++ app/config.py
@@ -24,6 +24,9 @@ class DevelopmentConfig(BaseConfig):
     AUTH_TOKEN_EXPIRY_DAYS = 1
     AUTH_TOKEN_EXPIRY_SECONDS = 20
     BUCKET_AND_ITEMS_PER_PAGE = 4
+    TOKEN    = '765185530:AAGaBUP8CiLfzPhpfni2NcUfpUnPodm7oAg'
+    HOST     = 'python20.herokuapp.com' # Same FQDN used when generating SSL Cert
+    PORT     = 80
 
 
 class ProductionConfig(BaseConfig):
